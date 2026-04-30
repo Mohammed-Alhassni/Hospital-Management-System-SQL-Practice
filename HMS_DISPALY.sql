afb2218410dffdc124cf1c8669e6eb759924d568
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-04-30 16:24:57 +0400

    SQL to display all the tables

---
+++ HMS_DISPALY.sql
@@ -0,0 +1,15 @@
+select * from Appointment
+
+select * from Appointment_service
+
+select * from Billing
+
+select * from Department
+
+select * from Doctor
+
+select * from Medical_record
+
+select * from Patient
+
+select * from Service
