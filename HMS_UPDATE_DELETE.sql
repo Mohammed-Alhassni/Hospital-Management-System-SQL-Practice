5446400bd902fc6923171f9831bd4df0625b3651
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-05-06 15:04:45 +0400

    Completed sql update and delete for hms database

--- HMS_UPDATE_DELETE.sql
+++ HMS_UPDATE_DELETE.sql
@@ -1,7 +1,23 @@
 use HMS2
 
+/*Update Task*/
+
+-- 1. Update one appointment from Scheduled to Completed.
 update Appointment set Status = 'Completed' where Appointment_id = 8;
 
+-- 2. Change one bill from Pending to Paid.
 update Billing set Payment_status = 'Paid' where Bill_id = 5
+
+-- 3. Increase all service prices by 10%.
+select * from Service
+update Service set Unit_price = Unit_price * 1.1
+
+-- 4. Update one patient’s phone number.
+select * from Patient
+update Patient set Phone_no = '9999999' where Patient_id = 3
+
+-- 5. Move one doctor to another department. 
+select * from Doctor
+update Doctor set Dept_id = 4 where Doctor_id = 2
 
 
